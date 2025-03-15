e9f3b5595515cf194aa5b555912b6ea75e3ec6d9
Evgenii-Perevalov <4077771@mail.ru>
2025-03-15 23:20:31 +0300

    version 2

--- project_1/game_v3.py
+++ project_1/game_v3.py
@@ -7,11 +7,7 @@ import numpy as np
 
 def game_core_v3(number: int = 1) -> int:
     """Угадываем число с мимнимальным количеством попыток.
-    
-    Комментарий. Можно было выбрать алгоритм деления выборки подбора случайных
-    чисел на 2 при каждой попытке, но интереснее было написать рекурсию с
-    уменьшением выборки, чтобы условие больше/меньше в полной мере выполнялось
-    
+        
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -21,21 +17,21 @@ def game_core_v3(number: int = 1) -> int:
     
     count = []
     
-    def count_number(number_from, number_to):
+    def count_number(selection):
         count.append(1)
-        # предполагаемое число
-        predict_number = np.random.randint(number_from, number_to)
+        # середина выборки
+        middle_number = (selection[-1]-selection[0])//2 + selection[0]
         
-        if predict_number == number:
+        if number == middle_number:
             result = len(count)
             return result # завершение функции при угадывании числа
-        elif predict_number > number:
-            # сокращаем выборку, если предполагаемое число больше или меньше
-            return count_number(number_from, predict_number + 1)
+        elif number < middle_number:
+            # делим выборку на 2 части и рассматриваем ту, где наше число
+            return count_number(list(range(selection[0],middle_number+1)))
         else:
-            return count_number(predict_number, number_to)
+            return count_number(list(range(middle_number+1,selection[-1]+1)))
     
-    result = count_number(number_from=1, number_to=101)
+    result = count_number(list(range(1,101)))
     return result
 
 
